569ca1fffe976ba7a6fc6fd782508c0f6a426a72
chandrakanth2096 <chandrakanth2096@gmail.com>
2022-09-17 10:43:10 +0000

    jobby-app

--- src/components/Jobs/index.js
+++ src/components/Jobs/index.js
@@ -144,11 +144,14 @@ class Jobs extends Component {
     ) : (
       <div className="no-jobs">
         <img
+          className="no-jobs-img"
           src="https://assets.ccbp.in/frontend/react-js/no-jobs-img.png"
           alt="no jobs"
         />
-        <h1>No Jobs Found</h1>
-        <p>We could not find any jobs. Try other filters</p>
+        <h1 className="no-jobs-heading">No Jobs Found</h1>
+        <p className="no-jobs-msg">
+          We could not find any jobs. Try other filters
+        </p>
       </div>
     )
   }
